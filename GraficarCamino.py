18da5a99eba1fe50c2e9d7b34c48a4e478312db5
Andres Bravo <BravoAndres@gmail.com>
2023-11-22 19:23:52 -0400

    segundoCommit

--- GraficarCamino.py
+++ GraficarCamino.py
@@ -1,7 +1,6 @@
 import cv2
 
 # def cargarGrafico(listaRecorrido):
-print(cv2.__version__)
 
 
 def cargarGrafico():
@@ -10,26 +9,41 @@ def cargarGrafico():
     # dist=0
     # tiempo=0
     try:
-        for i in range(50, 100):
-            img[133, i] = (0, 0, 255)
-            img[134, i] = (0, 0, 255)
-            img[135, i] = (0, 0, 255)
-            img[136, i] = (0, 0, 255)
-            img[137, i] = (0, 0, 255)
+        GraficarCuadrantes(img)
     except Exception:
         print("problemas al graficar")
 
-     # Escribir texto en una posición específica
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    posicion_texto = (50, 100)  # Posición (x, y) donde se colocará el texto
-    texto = "Texto de ejemplo"
-    color_texto = (0,0,0)  # Color del texto en formato BGR
-
-    cv2.putText(img, texto, posicion_texto, font, 1, color_texto, 2, cv2.LINE_AA)
+    
 
     cv2.imshow("img", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def GraficarCuadrantes(img):
+    try:
+        Main1(img)
+    except:
+        print("no grafico")
+
+def Main1(img):
+    pintarHorizontal(img,55,50,45,10)
+
+def pintarHorizontal(img,iniX,largo,iniY,ancho): 
+    try:
+        for i in range(iniX,iniX + largo):
+            for j in range (iniY,iniY+ancho):
+                img[j,i]=(0,0,255) 
+    except Exception:
+        print("error")
+    
+         
+def pintarvertical(img,iniX,largo,iniY,ancho): 
+    try:
+        for i in range(iniY,iniY + largo):
+            for j in range (iniX,iniX+ancho):
+                img[i,j]=(0,0,255) 
+    except Exception:
+        print("error")
 
 cargarGrafico()
 
