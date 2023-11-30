51fb0ee2a3ebee84bad3a1399ea44fe05d38dece
Andres Bravo <BravoAndres@gmail.com>
2023-11-30 16:35:49 -0400

    todoVertical

--- GraficarCamino.py
+++ GraficarCamino.py
@@ -21,44 +21,44 @@ def cargarGrafico():
     cv2.destroyAllWindows()
 
 def GraficarCuadrantes(img):
-    # Aroma1(img)
-    # Aroma2(img)
-    # Aroma3(img)
-    # Aroma4(img)
-    # Aroma5(img)
-    # Aroma6(img)
-    # Aroma7(img)
-    # Aroma8(img)
-    # Aroma9(img)
-    # Aroma10(img)
-    # Aroma11(img)
-    # Aroma12(img)
-    # Aroma13(img)
-    # Aroma14(img)
-    # Aroma15(img)
-    # Aroma16(img)
-    # Aroma17(img)
-    # Aroma18(img)
-    # Aroma19(img)
-    # Ayacucho1(img)
-    # Ayacucho2(img)
-    # Ayacucho3(img)
-    # Ayacucho4(img)
-    # Ayacucho5(img)
-    # Ayacucho6(img)
-    # Ayacucho7(img)
-    # Ayacucho8(img)
-    # Ayacucho9(img)
-    # Ayacucho10(img)
-    # Ayacucho11(img)
-    # Ayacucho12(img)
-    # Ayacucho13(img)
-    # Ayacucho14(img)
-    # Ayacucho15(img)
-    # Ayacucho16(img)
-    # Ayacucho17(img)
-    # Ayacucho18(img)
-    # Ayacucho19(img)
+    Aroma1(img)
+    Aroma2(img)
+    Aroma3(img)
+    Aroma4(img)
+    Aroma5(img)
+    Aroma6(img)
+    Aroma7(img)
+    Aroma8(img)
+    Aroma9(img)
+    Aroma10(img)
+    Aroma11(img)
+    Aroma12(img)
+    Aroma13(img)
+    Aroma14(img)
+    Aroma15(img)
+    Aroma16(img)
+    Aroma17(img)
+    Aroma18(img)
+    Aroma19(img)
+    Ayacucho1(img)
+    Ayacucho2(img)
+    Ayacucho3(img)
+    Ayacucho4(img)
+    Ayacucho5(img)
+    Ayacucho6(img)
+    Ayacucho7(img)
+    Ayacucho8(img)
+    Ayacucho9(img)
+    Ayacucho10(img)
+    Ayacucho11(img)
+    Ayacucho12(img)
+    Ayacucho13(img)
+    Ayacucho14(img)
+    Ayacucho15(img)
+    Ayacucho16(img)
+    Ayacucho17(img)
+    Ayacucho18(img)
+    Ayacucho19(img)
 
     AgustinLopez1(img)
     AgustinLopez2(img)
@@ -80,7 +80,74 @@ def GraficarCuadrantes(img):
     AgustinLopez18(img)
     AgustinLopez19(img)
     
+    NatanielAguirre1(img)
+    NatanielAguirre2(img)
+    NatanielAguirre3(img)
+    NatanielAguirre4(img)
+    NatanielAguirre5(img)
+    NatanielAguirre6(img)
+    NatanielAguirre7(img)
+    NatanielAguirre8(img)
+    NatanielAguirre9(img)
+    NatanielAguirre10(img)
+    NatanielAguirre11(img)
+    NatanielAguirre12(img)
+    NatanielAguirre13(img)
 
+    EstebanArce1(img)
+    EstebanArce2(img)
+    EstebanArce3(img)
+    EstebanArce4(img)
+    EstebanArce5(img)
+    EstebanArce6(img)
+    EstebanArce7(img)
+    EstebanArce8(img)
+    EstebanArce9(img)
+    EstebanArce10(img)
+    EstebanArce11(img)
+    EstebanArce12(img)
+    EstebanArce13(img)
+    EstebanArce14(img)
+    EstebanArce15(img)
+    EstebanArce16(img)
+    EstebanArce17(img)
+    EstebanArce18(img)
+    EstebanArce19(img)
+
+    Mayo1(img)
+    Mayo2(img)
+    Mayo3(img)
+    Mayo4(img)
+    Mayo5(img)
+    Mayo6(img)
+    Mayo7(img)
+    Mayo8(img)
+    Mayo9(img)
+    Mayo10(img)
+    Mayo11(img)
+    Mayo12(img)
+    Mayo13(img)
+
+    SanMartin1(img)
+    SanMartin2(img)
+    SanMartin3(img)
+    SanMartin4(img)
+    SanMartin5(img)
+    SanMartin6(img)
+    SanMartin7(img)
+    SanMartin8(img)
+    SanMartin9(img)
+    SanMartin10(img)
+    
+    Barrientos1(img)
+    Barrientos2(img)
+    Barrientos3(img)
+    Barrientos4(img)
+    Barrientos5(img)
+    Barrientos6(img)
+    Barrientos7(img)
+    Barrientos8(img)
+    Barrientos9(img)
 
 
 def Aroma1(img):
@@ -212,10 +279,151 @@ def AgustinLopez17(img):
     pintarvertical(img,135,25,690,20)
 def AgustinLopez18(img):
     pintarvertical(img,135,25,730,20)
-
 def AgustinLopez19(img):
     pintarvertical(img,135,25,765,20)
     
+def NatanielAguirre1(img):
+    pintarvertical(img,310,30,10,20)
+def NatanielAguirre2(img):
+    pintarvertical(img,310,30,50,20)
+
+def NatanielAguirre3(img):
+    pintarvertical(img,310,30,140,20)
+def NatanielAguirre4(img):
+    pintarvertical(img,310,30,190,20)
+def NatanielAguirre5(img):
+    pintarvertical(img,310,25,230,20)
+def NatanielAguirre6(img):
+    pintarvertical(img,310,25,270,20)
+def NatanielAguirre7(img):
+    pintarvertical(img,310,25,305,20)
+def NatanielAguirre8(img):
+    pintarvertical(img,310,25,340,20)
+def NatanielAguirre9(img):
+    pintarvertical(img,310,25,375,20)
+def NatanielAguirre10(img):
+    pintarvertical(img,310,25,420,20)
+
+def NatanielAguirre11(img):
+    pintarvertical(img,310,25,460,20)
+def NatanielAguirre12(img):
+    pintarvertical(img,310,25,495,20)
+def NatanielAguirre13(img):
+    pintarvertical(img,310,25,535,20)
+
+def EstebanArce1(img):
+    pintarvertical(img,485,30,10,20)
+""" +40 """
+def EstebanArce2(img):
+    pintarvertical(img,485,30,50,20)
+
+def EstebanArce3(img):
+    pintarvertical(img,485,30,140,20)
+def EstebanArce4(img):
+    pintarvertical(img,485,30,190,20)
+def EstebanArce5(img):
+    pintarvertical(img,485,25,230,20)
+def EstebanArce6(img):
+    pintarvertical(img,485,25,270,20)
+def EstebanArce7(img):
+    pintarvertical(img,485,25,305,20)
+def EstebanArce8(img):
+    pintarvertical(img,485,25,340,20)
+def EstebanArce9(img):
+    pintarvertical(img,485,25,375,20)
+def EstebanArce10(img):
+    pintarvertical(img,485,25,420,20)
+
+def EstebanArce11(img):
+    pintarvertical(img,485,25,460,20)
+def EstebanArce12(img):
+    pintarvertical(img,485,25,495,20)
+def EstebanArce13(img):
+    pintarvertical(img,485,25,535,20)
+def EstebanArce14(img):
+    pintarvertical(img,485,25,570,20)
+
+def EstebanArce15(img):
+    pintarvertical(img,485,25,610,20)
+def EstebanArce16(img):
+    pintarvertical(img,485,25,650,20)
+def EstebanArce17(img):
+    pintarvertical(img,485,25,690,20)
+def EstebanArce18(img):
+    pintarvertical(img,485,25,730,20)
+def EstebanArce19(img):
+    pintarvertical(img,485,25,765,20)
+
+def Mayo1(img):
+    pintarvertical(img,770,30,10,20)
+def Mayo2(img):
+    pintarvertical(img,770,30,50,20)
+
+def Mayo3(img):
+    pintarvertical(img,770,30,140,20)
+def Mayo4(img):
+    pintarvertical(img,770,30,190,20)
+def Mayo5(img):
+    pintarvertical(img,770,25,230,20)
+def Mayo6(img):
+    pintarvertical(img,770,25,270,20)
+def Mayo7(img):
+    pintarvertical(img,770,25,305,20)
+def Mayo8(img):
+    pintarvertical(img,770,25,340,20)
+def Mayo9(img):
+    pintarvertical(img,770,25,375,20)
+def Mayo10(img):
+    pintarvertical(img,770,25,420,20)
+
+def Mayo11(img):
+    pintarvertical(img,770,25,460,20)
+def Mayo12(img):
+    pintarvertical(img,770,25,495,20)
+def Mayo13(img):
+    pintarvertical(img,770,25,535,20)
+    
+def SanMartin1(img):
+    pintarvertical(img,1000,30,10,20)
+def SanMartin2(img):
+    pintarvertical(img,1000,30,50,20)
+
+def SanMartin3(img):
+    pintarvertical(img,1000,30,140,20)
+def SanMartin4(img):
+    pintarvertical(img,1000,30,190,20)
+def SanMartin5(img):
+    pintarvertical(img,1000,25,230,20)
+def SanMartin6(img):
+    pintarvertical(img,1000,25,270,20)
+def SanMartin7(img):
+    pintarvertical(img,1000,25,305,20)
+def SanMartin8(img):
+    pintarvertical(img,1000,25,340,20)
+def SanMartin9(img):
+    pintarvertical(img,1000,25,375,20)
+def SanMartin10(img):
+    pintarvertical(img,1000,25,420,20)
+
+def Barrientos1(img):
+    pintarvertical(img,1000,25,460,20)
+def Barrientos2(img):
+    pintarvertical(img,1000,25,495,20)
+def Barrientos3(img):
+    pintarvertical(img,1000,25,535,20)
+def Barrientos4(img):
+    pintarvertical(img,1000,25,570,20)
+
+def Barrientos5(img):
+    pintarvertical(img,1000,25,610,20)
+def Barrientos6(img):
+    pintarvertical(img,1000,25,650,20)
+def Barrientos7(img):
+    pintarvertical(img,1000,25,690,20)
+def Barrientos8(img):
+    pintarvertical(img,1000,25,730,20)
+def Barrientos9(img):
+    pintarvertical(img,1000,25,765,20)
 
 
 
