45ce9d8173d5fe1f3d3b98dcf5270d8301fa8795
Andres Bravo <BravoAndres@gmail.com>
2023-11-30 15:50:30 -0400

    pintandoCalles

--- GraficarCamino.py
+++ GraficarCamino.py
@@ -5,33 +5,246 @@ print(cv2.__version__)
 
 
 def cargarGrafico():
-    img = cv2.imread("MapaIA6.png")
+    img = cv2.imread("MapaCocha1.png")
     # lista=listaRecorrido
     # dist=0
     # tiempo=0
     try:
-        for i in range(50, 100):
-            img[133, i] = (0, 0, 255)
-            img[134, i] = (0, 0, 255)
-            img[135, i] = (0, 0, 255)
-            img[136, i] = (0, 0, 255)
-            img[137, i] = (0, 0, 255)
+       GraficarCuadrantes(img)
     except Exception:
         print("problemas al graficar")
 
-     # Escribir texto en una posición específica
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    posicion_texto = (50, 100)  # Posición (x, y) donde se colocará el texto
-    texto = "Texto de ejemplo"
-    color_texto = (0,0,0)  # Color del texto en formato BGR
-
-    cv2.putText(img, texto, posicion_texto, font, 1, color_texto, 2, cv2.LINE_AA)
+    
 
     cv2.imshow("img", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+def GraficarCuadrantes(img):
+    # Aroma1(img)
+    # Aroma2(img)
+    # Aroma3(img)
+    # Aroma4(img)
+    # Aroma5(img)
+    # Aroma6(img)
+    # Aroma7(img)
+    # Aroma8(img)
+    # Aroma9(img)
+    # Aroma10(img)
+    # Aroma11(img)
+    # Aroma12(img)
+    # Aroma13(img)
+    # Aroma14(img)
+    # Aroma15(img)
+    # Aroma16(img)
+    # Aroma17(img)
+    # Aroma18(img)
+    # Aroma19(img)
+    # Ayacucho1(img)
+    # Ayacucho2(img)
+    # Ayacucho3(img)
+    # Ayacucho4(img)
+    # Ayacucho5(img)
+    # Ayacucho6(img)
+    # Ayacucho7(img)
+    # Ayacucho8(img)
+    # Ayacucho9(img)
+    # Ayacucho10(img)
+    # Ayacucho11(img)
+    # Ayacucho12(img)
+    # Ayacucho13(img)
+    # Ayacucho14(img)
+    # Ayacucho15(img)
+    # Ayacucho16(img)
+    # Ayacucho17(img)
+    # Ayacucho18(img)
+    # Ayacucho19(img)
+
+    AgustinLopez1(img)
+    AgustinLopez2(img)
+    AgustinLopez3(img)
+    AgustinLopez4(img)
+    AgustinLopez5(img)
+    AgustinLopez6(img)
+    AgustinLopez7(img)
+    AgustinLopez8(img)
+    AgustinLopez9(img)
+    AgustinLopez10(img)
+    AgustinLopez11(img)
+    AgustinLopez12(img)
+    AgustinLopez13(img)
+    AgustinLopez14(img)
+    AgustinLopez15(img)
+    AgustinLopez16(img)
+    AgustinLopez17(img)
+    AgustinLopez18(img)
+    AgustinLopez19(img)
+    
+
+
+
+def Aroma1(img):
+    pintarHorizontal(img,10,50,90,20)
+""" + 55 """
+def Aroma2(img):
+    pintarHorizontal(img,65,50,90,20)
+
+def Aroma3(img):
+    pintarHorizontal(img,120,50,90,20) 
+""" + 60 """
+def Aroma4(img):
+    pintarHorizontal(img,180,50,90,20)
+def Aroma5(img):
+    pintarHorizontal(img,240,50,90,20)
+def Aroma6(img):
+    pintarHorizontal(img,295,50,90,20)
+""" + 60 """ 
+def Aroma7(img):
+    pintarHorizontal(img,355,50,90,20)
+def Aroma8(img):
+    pintarHorizontal(img,410,50,90,20)
+def Aroma9(img):
+    pintarHorizontal(img,465,50,90,20)
+""" + 60 """ 
+def Aroma10(img):
+    pintarHorizontal(img,525,50,90,20)
+def Aroma11(img):
+    pintarHorizontal(img,585,50,90,20)
+def Aroma12(img):
+    pintarHorizontal(img,645,50,90,20)
+""" + 55 """ 
+def Aroma13(img):
+    pintarHorizontal(img,700,50,90,20)
+def Aroma14(img):
+    pintarHorizontal(img,755,50,90,20)
+def Aroma15(img):
+    pintarHorizontal(img,810,50,90,20)
+""" + 55 """ 
+def Aroma16(img):
+    pintarHorizontal(img,865,50,90,20)
+def Aroma17(img):
+    pintarHorizontal(img,920,50,90,20)
+def Aroma18(img):
+    pintarHorizontal(img,1040,50,90,20)
+def Aroma19(img):
+    pintarHorizontal(img,1095,50,90,20)
+
+def Ayacucho1(img):
+    pintarvertical(img,25,30,10,20)
+""" +40 """
+def Ayacucho2(img):
+    pintarvertical(img,25,30,50,20)
+
+def Ayacucho3(img):
+    pintarvertical(img,25,30,140,20)
+def Ayacucho4(img):
+    pintarvertical(img,25,30,190,20)
+def Ayacucho5(img):
+    pintarvertical(img,25,25,230,20)
+def Ayacucho6(img):
+    pintarvertical(img,25,25,270,20)
+def Ayacucho7(img):
+    pintarvertical(img,25,25,305,20)
+def Ayacucho8(img):
+    pintarvertical(img,25,25,340,20)
+def Ayacucho9(img):
+    pintarvertical(img,25,25,375,20)
+def Ayacucho10(img):
+    pintarvertical(img,25,25,420,20)
+
+def Ayacucho11(img):
+    pintarvertical(img,25,25,460,20)
+def Ayacucho12(img):
+    pintarvertical(img,25,25,495,20)
+def Ayacucho13(img):
+    pintarvertical(img,25,25,535,20)
+def Ayacucho14(img):
+    pintarvertical(img,25,25,570,20)
+
+def Ayacucho15(img):
+    pintarvertical(img,25,25,610,20)
+def Ayacucho16(img):
+    pintarvertical(img,25,25,650,20)
+def Ayacucho17(img):
+    pintarvertical(img,25,25,690,20)
+def Ayacucho18(img):
+    pintarvertical(img,25,25,730,20)
+
+def Ayacucho19(img):
+    pintarvertical(img,25,25,765,20)
+
+def AgustinLopez1(img):
+    pintarvertical(img,135,30,10,20)
+def AgustinLopez2(img):
+    pintarvertical(img,135,30,50,20)
+
+def AgustinLopez3(img):
+    pintarvertical(img,135,30,140,20)
+def AgustinLopez4(img):
+    pintarvertical(img,135,30,190,20)
+def AgustinLopez5(img):
+    pintarvertical(img,135,25,230,20)
+def AgustinLopez6(img):
+    pintarvertical(img,135,25,270,20)
+def AgustinLopez7(img):
+    pintarvertical(img,135,25,305,20)
+def AgustinLopez8(img):
+    pintarvertical(img,135,25,340,20)
+def AgustinLopez9(img):
+    pintarvertical(img,135,25,375,20)
+def AgustinLopez10(img):
+    pintarvertical(img,135,25,420,20)
+
+def AgustinLopez11(img):
+    pintarvertical(img,135,25,460,20)
+def AgustinLopez12(img):
+    pintarvertical(img,135,25,495,20)
+def AgustinLopez13(img):
+    pintarvertical(img,135,25,535,20)
+def AgustinLopez14(img):
+    pintarvertical(img,135,25,570,20)
+
+def AgustinLopez15(img):
+    pintarvertical(img,135,25,610,20)
+def AgustinLopez16(img):
+    pintarvertical(img,135,25,650,20)
+def AgustinLopez17(img):
+    pintarvertical(img,135,25,690,20)
+def AgustinLopez18(img):
+    pintarvertical(img,135,25,730,20)
+
+def AgustinLopez19(img):
+    pintarvertical(img,135,25,765,20)
+    
+
+
+
+def pintarHorizontal(img,iniX,largo,iniY,ancho): 
+    try:
+        for i in range(iniX,iniX + largo):
+            for j in range (iniY,iniY+ancho):
+                img[j,i]=(0,0,255) 
+    except Exception:
+        print("error")
+    
+         
+def pintarvertical(img,iniX,largo,iniY,ancho): 
+    try:
+        for i in range(iniY,iniY + largo):
+            for j in range (iniX,iniX+ancho):
+                img[i,j]=(0,0,255) 
+    except Exception:
+        print("error")
+
 cargarGrafico()
 
 # pip install opencv-python tienes que instalar
 
+
+ # Escribir texto en una posición específica
+    # font = cv2.FONT_HERSHEY_SIMPLEX
+    # posicion_texto = (50, 100)  # Posición (x, y) donde se colocará el texto
+    # texto = "Texto de ejemplo"
+    # color_texto = (0,0,0)  # Color del texto en formato BGR
+
+    # cv2.putText(img, texto, posicion_texto, font, 1, color_texto, 2, cv2.LINE_AA)
